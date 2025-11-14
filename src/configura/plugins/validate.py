aad2270639db87fb36ae0ed859be9aea278a54de
Kacper <remiszkacp@gmail.com>
2025-11-14 17:32:03 +0100

    create(plugin): validate

---
+++ src/configura/plugins/validate.py
@@ -0,0 +1,33 @@
+
+from configura.adapters.helper_adapter import validate
+from configura.constants import *
+
+class Validate:
+    def __init__(
+        self,
+        data: TYPE_DATA,
+        schema_path: str,
+        schema_encoding: str = DEFAULT_ENCODING,
+        on_fail: TYPE_ON_FAIL = "skip",
+        dlq_path: str = "",
+        dlq_format: TYPE_DLQ_FORMAT = "json",
+        dlq_encoding: str = DEFAULT_ENCODING
+    ) -> None:
+        self.data = data
+        self.schema_path = schema_path
+        self.schema_encoding = schema_encoding
+        self.on_fail: TYPE_ON_FAIL= on_fail
+        self.dlq_path = dlq_path
+        self.dlq_format: TYPE_DLQ_FORMAT = dlq_format
+        self.dlq_encoding = dlq_encoding
+    
+    def process(self, data):
+        return validate(
+            data,
+            self.schema_path,
+            schema_encoding=self.schema_encoding,
+            on_fail=self.on_fail,
+            dlq_path=self.dlq_path,
+            dlq_format=self.dlq_format,
+            dlq_encoding=self.dlq_encoding
+        )
